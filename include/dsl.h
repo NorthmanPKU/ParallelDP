1df127e5cacde8fef8eeabd9523ccea5acc4aacd
NorthmanPKU <jjn20020813@163.com>
2025-04-26 22:10:05 -0400

    dsl lis and lcs finish

--- include/dsl.h
+++ include/dsl.h
@@ -14,7 +14,6 @@
 
 namespace dp_dsl {
 
-
 // Enumeration for different DP problem types
 enum class ProblemType {
     LIS,
@@ -29,42 +28,246 @@ enum class Objective {
     MINIMIZE
 };
 
+
+
+enum class ConstraintType { 
+    LESS_THAN, 
+    GREATER_THAN, 
+    EQUAL, 
+    NOT_EQUAL,
+    NONE
+};
 // State space definition
-struct StateVariable {
-    std::string name;
+
+struct Var {
+    enum class VarType {
+        IND,
+        SINGLE_DEP,
+        RANGE_DEP
+    };
+
+    VarType type;
+    Var(VarType t) : type(t) {}
+
+    virtual ~Var() = default;
+};
+
+struct SingleDepVar;
+
+struct IndVar: Var {
     int min_value;
     int max_value;
     
-    StateVariable(const std::string& n, int min, int max) 
-        : name(n), min_value(min), max_value(max) {}
+    IndVar(int min, int max) 
+        : Var(VarType::IND), min_value(min), max_value(max) {}
+
+    SingleDepVar* operator+(int v);
+    SingleDepVar* operator-(int v);
 };
 
+struct SingleDepVar: Var {
+    IndVar* base;
+    int offset;
+
+    SingleDepVar(IndVar* base, int offset)
+        : Var(VarType::SINGLE_DEP), base(base), offset(offset) {}
+};
+
+struct RangeDepVar: Var {
+    enum class RangeType {
+        LIRV, //left int right value
+        LVRI,
+        LVRV
+    };
+
+    Var* min_var;
+    Var* max_var;
+    int min_val;
+    int max_val;
+    RangeType range_type;
+
+    RangeDepVar(Var* min_var, Var* max_var) 
+        : Var(VarType::RANGE_DEP), min_var(min_var), max_var(max_var), range_type(RangeType::LVRV) {}
+
+    RangeDepVar(int min_val, Var* max_var)
+        : Var(VarType::RANGE_DEP), max_var(max_var), min_val(min_val), range_type(RangeType::LIRV) {}
+
+    RangeDepVar(Var* min_var, int max_val)
+        : Var(VarType::RANGE_DEP), min_var(min_var), max_val(max_val), range_type(RangeType::LVRI) {}
+};
+
+SingleDepVar* IndVar::operator+(int v) {
+    SingleDepVar* res = new SingleDepVar(this, v);
+    return res;
+}
+
+SingleDepVar* IndVar::operator-(int v) {
+    SingleDepVar* res = new SingleDepVar(this, -v);
+    return res;
+}
+
+SingleDepVar* add(IndVar* v, int c) {
+    SingleDepVar* res = new SingleDepVar(v, c);
+    return res;
+}
+
+SingleDepVar* minus(IndVar* v, int c) {
+    SingleDepVar* res = new SingleDepVar(v, -c);
+    return res;
+}
+
+template<typename T> struct Sequence;
+
+template<typename T> struct Val;
 // Constraint definition
+template<typename T>
 struct Constraint {
-    enum class ConstraintType { LESS_THAN, GREATER_THAN, EQUAL, NOT_EQUAL };
-    
-    std::string var1;
-    std::string var2;
+    Val<T> val1;
+    Val<T> val2;
     ConstraintType type;
     
-    Constraint(const std::string& v1, const std::string& v2, ConstraintType t)
-        : var1(v1), var2(v2), type(t) {}
+    Constraint(Val<T> v1, Val<T> v2, ConstraintType t)
+        : val1(v1), val2(v2), type(t) {}
+
+    // Constraint()
+    //     : val1(nullptr), val2(nullptr), type(ConstraintType::NONE) {}
+    Constraint()
+        : val1(Val<T>(nullptr, nullptr)), val2(Val<T>(nullptr, nullptr)), type(ConstraintType::NONE) {}
 };
+
+template<typename T>
+struct Val {
+    Sequence<T>* seq;
+    Var* idx;
+
+    Val(Sequence<T>* s, Var* i)
+        : seq(s), idx(i) {}
+    
+    Constraint<T> operator<(Val<T> v) {
+        return Constraint<T>(*this, v, ConstraintType::LESS_THAN);
+    }
+
+    Constraint<T> operator>(Val<T> v) {
+        return Constraint<T>(*this, v, ConstraintType::GREATER_THAN);
+    }
+
+    Constraint<T> operator==(Val<T> v) {
+        return Constraint<T>(*this, v, ConstraintType::EQUAL);
+    }
+
+    Constraint<T> operator!=(Val<T> v) {
+        return Constraint<T>(*this, v, ConstraintType::NOT_EQUAL);
+    }
+};
+
+template<typename T>
+struct Sequence {
+    std::vector<T> data;
+
+    Sequence(const std::vector<T>& d) : data(d) {}
+
+    Val<T> operator[](Var* v) {
+        return Val<T>(this, v);
+    }
+
+    Val<T> operator[](SingleDepVar* v) {
+
+        return Val<T>(this, dynamic_cast<Var*>(v));
+    }
+};
+
+template<typename T>
+Val<T> index(Sequence<T>* seq, SingleDepVar* v) {
+    return Val<T>(seq, dynamic_cast<Var*>(v));
+}
+
+enum class ExpressionType {
+    MAX,
+    MIN,
+    NUMBER,
+    STATUS,
+    NONE
+};
+
+struct Expression {
+    ExpressionType type;
+
+    Expression(ExpressionType t) : type(t) {}
+    Expression() : type(ExpressionType::NONE) {}
+};
+
+struct TwoPartExpression : Expression {
+    Expression left;
+    Expression right;
+
+    TwoPartExpression(ExpressionType t, Expression l, Expression r) : Expression(t), left(l), right(r) {}
+};
+
+struct Number : TwoPartExpression {
+    int value;
+
+    Number(int v) : TwoPartExpression(ExpressionType::NUMBER, Expression(), Expression()), value(v) {}
+};
+
+struct Status : Expression {
+    // with unknown number of Vars
+    std::vector<Var*> vars;
+    int dim = 0;
+
+    int constant = 0;
+
+    Status(Var* v1) : Expression(ExpressionType::STATUS), vars({v1}), dim(1) {}
+    Status(Var* v1, Var* v2) : Expression(ExpressionType::STATUS), vars({v1, v2}), dim(2) {}
+
+    Status operator+(int c) const {
+        Status res = *this;
+        res.constant += c;
+        return res;
+    }
+
+    Status operator-(int c) const {
+        Status res = *this;
+        res.constant -= c;
+        return res;
+    }
+};
+
+TwoPartExpression max(Expression s1, Expression s2) {
+    TwoPartExpression res(ExpressionType::MAX, s1, s2);
+    return res;
+}
 
 // Data input representation
 // Base class for defining DP problems
+template<typename U>
 class DPProblem {
 public:
     virtual ~DPProblem() = default;
 
     // Problem definition methods
-    void addStateVariable(const std::string& name, int min, int max) {
-        state_variables.emplace_back(name, min, max);
+    void addVar(Var* v) {
+        if (v->type == Var::VarType::IND) {
+            status_dim++;
+            state_variables.emplace_back(dynamic_cast<IndVar*>(v));
+            std::cout << "Independent variable added" << std::endl;
+        } else if (v->type == Var::VarType::SINGLE_DEP) {
+            // status_dim++;
+            // state_variables.emplace_back(v);
+            std::cout << "Single dependency variable added" << std::endl;
+        } else if (v->type == Var::VarType::RANGE_DEP) {
+            // status_dim++;
+            // state_variables.emplace_back(v);
+            std::cout << "Range dependency variable added" << std::endl;
+        }
+    }
+    
+    template<typename T>
+    void addCondition(Constraint<T> c, Expression s) {
+        conditions.emplace_back(c, s);
     }
 
-    void addConstraint(const std::string& var1, const std::string& var2, 
-                      Constraint::ConstraintType type) {
-        constraints.emplace_back(var1, var2, type);
+    void addCondition(Expression s) {
+        conditions.emplace_back(s);
     }
 
     void setObjective(Objective obj) {
@@ -77,8 +280,8 @@ public:
 
     // Data input methods
     template<typename T>
-    void addSequence(const std::string& name, const std::vector<T>& data) {
-        data_map[name] = data;
+    void addSequence(Sequence<T>* seq) {
+        sequences.emplace_back(seq);
     }
 
     template<typename T>
@@ -111,34 +314,43 @@ public:
     // Problem recognition
     ProblemType getProblemType() const {
         // LIS recognition pattern
-        if (state_variables.size() == 1 && objective == Objective::MAXIMIZE) {
-            for (const auto& constraint : constraints) {
-                if (constraint.type == Constraint::ConstraintType::LESS_THAN &&
-                    (constraint.var1 == "j" && constraint.var2 == "i") ||
-                    (constraint.var1 == "prev" && constraint.var2 == "curr")) {
-                    return ProblemType::LIS;
-                }
-            }
+        if (state_variables.size() == 1) {
+            return ProblemType::LIS;
         }
+        if (state_variables.size() == 2) {
+            return ProblemType::LCS;
+        }
+        // if (state_variables.size() == 1 && objective == Objective::MAXIMIZE) {
+        //     for (const auto& constraint : constraints) {
+        //         if (constraint.type == Constraint::ConstraintType::LESS_THAN &&
+        //             (constraint.var1 == "j" && constraint.var2 == "i") ||
+        //             (constraint.var1 == "prev" && constraint.var2 == "curr")) {
+        //             return ProblemType::LIS;
+        //         }
+        //     }
+        // }
 
-        // LCS recognition pattern
-        if (state_variables.size() == 2 && objective == Objective::MAXIMIZE) {
-            if (hasSequence("seq1") && hasSequence("seq2")) {
-                return ProblemType::LCS;
-            }
-        }
+        // // LCS recognition pattern
+        // if (state_variables.size() == 2 && objective == Objective::MAXIMIZE) {
+        //     if (hasSequence("seq1") && hasSequence("seq2")) {
+        //         return ProblemType::LCS;
+        //     }
+        // }
 
-        // Convex GLWS recognition pattern
-        if (state_variables.size() <= 2 && objective == Objective::MINIMIZE) {
-            return ProblemType::CONVEX_GLWS;
-        }
+        // // Convex GLWS recognition pattern
+        // if (state_variables.size() <= 2 && objective == Objective::MINIMIZE) {
+        //     return ProblemType::CONVEX_GLWS;
+        // }
 
         return ProblemType::UNKNOWN;
     }
 
 protected:
-    std::vector<StateVariable> state_variables;
-    std::vector<Constraint> constraints;
+    int status_dim = 0;
+    std::vector<IndVar*> state_variables;
+    std::vector<Sequence<U>*> sequences;
+    // std::vector<Constraint> constraints;
+    std::vector<std::pair<Constraint<U>, Expression>> conditions;
     Objective objective = Objective::MAXIMIZE;
     std::function<void(const std::map<std::string, int>&)> recurrence_func;
     using DataType = std::variant<std::vector<int>, std::vector<long double>, std::vector<std::string>, long double, int, std::string>;
@@ -149,7 +361,7 @@ protected:
 class SolverDispatcher {
 public:
     template<typename T>
-    static T solve(DPProblem& problem) {
+    static T solve(DPProblem<T>& problem) {
         ProblemType type = problem.getProblemType();
         
         switch (type) {
@@ -166,18 +378,18 @@ public:
     
 private:
     template<typename T>
-    static T solveLIS(DPProblem& problem) {
-        const auto& sequence = problem.getSequence<int>("sequence");
+    static T solveLIS(DPProblem<T>& problem) {
+        const auto& sequence = problem.template getSequence<T>("sequence");
         
         // Create backend solver
-        LIS<int> solver;
+        LIS<T> solver;
         return solver.compute(sequence);
     }
     
     template<typename T>
-    static T solveLCS(DPProblem& problem) {
-        const auto& seq1 = problem.getSequence<int>("seq1");
-        const auto& seq2 = problem.getSequence<int>("seq2");
+    static T solveLCS(DPProblem<T>& problem) {
+        const auto& seq1 = problem.template getSequence<T>("seq1");
+        const auto& seq2 = problem.template getSequence<T>("seq2");
         
         // Create backend solver
         LCS<int> solver;
@@ -185,9 +397,9 @@ private:
     }
     
     template<typename T>
-    static T solveConvexGLWS(DPProblem& problem) {
-        const auto& sequence = problem.getSequence<T>("data");
-        const auto& buildCost = problem.getValue<T>("buildCost");
+    static T solveConvexGLWS(DPProblem<T>& problem) {
+        const auto& sequence = problem.template getSequence<T>("data");
+        const auto& buildCost = problem.template getValue<T>("buildCost");
 
         auto costFunc = [&](int j, int i, const std::vector<T>& pos) -> T {  // [j+1, i]
             if (i - j < 1) return buildCost;
@@ -215,17 +427,26 @@ public:
         return ProblemBuilder();
     }
     
-    ProblemBuilder& withStateVariable(const std::string& name, int min, int max) {
-        problem_.addStateVariable(name, min, max);
+    ProblemBuilder& withVar(Var* v) {
+        problem_.addVar(v);
+        return *this;
+    }
+
+    ProblemBuilder& withVar(int min, Var max) {
+        problem_.addDepVar(min, max);
         return *this;
     }
     
-    ProblemBuilder& withConstraint(const std::string& var1, const std::string& var2, 
-                                 Constraint::ConstraintType type) {
-        problem_.addConstraint(var1, var2, type);
+    ProblemBuilder& withCondition(Constraint<T> c, Expression s) {
+        problem_.addCondition(c, s);
         return *this;
     }
-    
+
+    ProblemBuilder& withCondition(Expression s) {
+        problem_.addCondition(Constraint<T>(), s);
+        return *this;
+    }
+
     ProblemBuilder& withObjective(Objective obj) {
         problem_.setObjective(obj);
         return *this;
@@ -236,8 +457,8 @@ public:
         return *this;
     }
     
-    ProblemBuilder& withSequence(const std::string& name, const std::vector<T>& data) {
-        problem_.addSequence(name, data);
+    ProblemBuilder& withSequence(Sequence<T>* seq) {
+        problem_.addSequence(seq);
         return *this;
     }
 
@@ -246,47 +467,47 @@ public:
         return *this;
     }
     
-    DPProblem build() {
+    DPProblem<T> build() {
         return std::move(problem_);
     }
     
 private:
-    DPProblem problem_;
+    DPProblem<T> problem_;
 };
 
 // Convenience factory methods for common problems
-class CommonProblems {
-public:
-    static DPProblem createLIS(const std::vector<int>& sequence) {
-        return ProblemBuilder<int>::create()
-            .withStateVariable("i", 0, sequence.size())
-            .withConstraint("j", "i", Constraint::ConstraintType::LESS_THAN)
-            .withObjective(Objective::MAXIMIZE)
-            .withSequence("sequence", sequence)
-            .build();
-    }
+// class CommonProblems {
+// public:
+//     static DPProblem<int> createLIS(const std::vector<int>& sequence) {
+//         return ProblemBuilder<int>::create()
+//             .withVar("i", 0, sequence.size())
+//             .withConstraint("j", "i", ConstraintType::LESS_THAN)
+//             .withObjective(Objective::MAXIMIZE)
+//             .withSequence("sequence", sequence)
+//             .build();
+//     }
     
-    static DPProblem createLCS(const std::vector<int>& seq1, const std::vector<int>& seq2) {
-        return ProblemBuilder<int>::create()
-            .withStateVariable("i", 0, seq1.size())
-            .withStateVariable("j", 0, seq2.size())
-            .withObjective(Objective::MAXIMIZE)
-            .withSequence("seq1", seq1)
-            .withSequence("seq2", seq2)
-            .build();
-    }
+//     static DPProblem<int> createLCS(const std::vector<int>& seq1, const std::vector<int>& seq2) {
+//         return ProblemBuilder<int>::create()
+//             .withVar("i", 0, seq1.size())
+//             .withVar("j", 0, seq2.size())
+//             .withObjective(Objective::MAXIMIZE)
+//             .withSequence("seq1", seq1)
+//             .withSequence("seq2", seq2)
+//             .build();
+//     }
     
-    static DPProblem createConvexGLWS(const std::vector<long double>& data, 
-                                    std::function<double(int, int)> cost_func) {
-        auto problem = ProblemBuilder<long double>::create()
-            .withStateVariable("i", 0, data.size())
-            .withObjective(Objective::MINIMIZE)
-            .withSequence("data", data)
-            .build();
+//     static DPProblem<long double> createConvexGLWS(const std::vector<long double>& data, 
+//                                     std::function<double(int, int)> cost_func) {
+//         auto problem = ProblemBuilder<long double>::create()
+//             .withVar("i", 0, data.size())
+//             .withObjective(Objective::MINIMIZE)
+//             .withSequence("data", data)
+//             .build();
             
-        // Store cost function (would need to be extended in actual implementation)
-        return problem;
-    }
-};
+//         // Store cost function (would need to be extended in actual implementation)
+//         return problem;
+//     }
+// };
 
 } // namespace dp_dsl
--- tests/test_dsl.cpp
+++ tests/test_dsl.cpp
@@ -6,68 +6,63 @@ int main() {
     using namespace dp_dsl;
     
     // Example 1: Defining LIS problem using builder pattern
-    auto lis_problem = ProblemBuilder<int>::create()
-        .withStateVariable("i", 0, 10)
-        .withConstraint("j", "i", Constraint::ConstraintType::LESS_THAN)
-        .withObjective(Objective::MAXIMIZE)
-        .withSequence("sequence", {3, 1, 4, 2, 7, 5, 8, 6, 9, 10})
-        .withRecurrence([](const std::map<std::string, int>& state) {
-            // Recurrence relation: dp[i] = max(dp[j] + 1) for all j < i where seq[j] < seq[i]
-            // This would be interpreted by the backend
-        })
+    auto Seq = new Sequence<int>({3, 1, 4, 2, 7, 5, 8, 6, 9, 10});
+    auto I = new IndVar(0, 10);
+    auto J = new RangeDepVar(0, I-1);
+    auto problem1 = ProblemBuilder<int>::create()
+        .withVar(I)
+        .withVar(J)
+        .withSequence(Seq)
+        .withCondition(dp_dsl::max(Status(J) + 1, Status(I)))
         .build();
-    
-    // Let the system recognize the problem type
-    ProblemType type = lis_problem.getProblemType();
+    // // Let the system recognize the problem type
+    ProblemType type = problem1.getProblemType();
     std::cout << "Detected problem type: " << 
         (type == ProblemType::LIS ? "LIS" : 
          type == ProblemType::LCS ? "LCS" : 
          type == ProblemType::CONVEX_GLWS ? "Convex GLWS" : "Unknown") 
         << std::endl;
     
-    // Solve the problem
-    int lis_result = SolverDispatcher::solve<int>(lis_problem);
-    std::cout << "LIS result: " << lis_result << std::endl;
-    
-    // Example 2: Using convenience factory for LCS
-    std::vector<int> seq1 = {1, 2, 3, 4, 5};
-    std::vector<int> seq2 = {3, 1, 4, 2, 5};
-    
-    auto lcs_problem = CommonProblems::createLCS(seq1, seq2);
-    int lcs_result = SolverDispatcher::solve<int>(lcs_problem);
-    std::cout << "LCS result: " << lcs_result << std::endl;
-    
-    // Example 3: Manual problem definition for Convex GLWS
-    auto glws_problem = ProblemBuilder<long double>::create()
-        .withStateVariable("i", 0, 20)
-        .withStateVariable("j", 0, 20)
-        .withObjective(Objective::MINIMIZE)
-        .withConstraint("i", "j", Constraint::ConstraintType::LESS_THAN)
-        .withSequence("data", std::vector<long double>{1.0, 2.5, 3.0, 4.2, 5.1, 6.3})
-        .withValue("buildCost", 10.0)
-        .withRecurrence([](const std::map<std::string, int>& state) {
-            // Recurrence relation for convex GLWS
-            // dp[i][j] = min(dp[i][k] + cost(k, j)) for all k in [i, j)
-        })
+
+
+    auto Seq1 = new Sequence<int>({1, 2, 3, 4, 5});
+    auto Seq2 = new Sequence<int>({3, 1, 4, 2, 5});
+    auto I2 = new IndVar(0, 5);
+    auto J2 = new IndVar(0, 5);
+
+    auto problem2 = ProblemBuilder<int>::create()
+        .withVar(I2)
+        .withVar(J2)
+        .withSequence(Seq1)
+        .withSequence(Seq2)
+        .withCondition(index(Seq1, minus(I2, 1)) == index(Seq2, minus(J2, 1)), Status(I2, J2) + 1)
+        .withCondition(index(Seq1, minus(I2, 1)) != index(Seq2, minus(J2, 1)), 
+                    dp_dsl::max(Status(I2, minus(J2, 1)), Status(minus(I2, 1), J2)))
         .build();
     
-    double glws_result = SolverDispatcher::solve<long double>(glws_problem);
-    std::cout << "Convex GLWS result: " << glws_result << std::endl;
-    
-    // Example 4: Generic problem definition that will be analyzed
-    auto generic_problem = ProblemBuilder<int>::create()
-        .withStateVariable("position", 0, 100)
-        .withObjective(Objective::MAXIMIZE)
-        .withSequence("values", {10, 20, 30, 15, 25, 35})
-        .build();
-    
-    // The system will analyze the structure and determine what kind of problem this is
-    ProblemType generic_type = generic_problem.getProblemType();
-    std::cout << "Generic problem detected as: " << 
-        (generic_type == ProblemType::LIS ? "LIS" : 
-         generic_type == ProblemType::LCS ? "LCS" : 
-         generic_type == ProblemType::CONVEX_GLWS ? "Convex GLWS" : "Unknown") 
+    ProblemType type2 = problem2.getProblemType();
+    std::cout << "Detected problem type: " << 
+        (type2 == ProblemType::LIS ? "LIS" : 
+         type2 == ProblemType::LCS ? "LCS" : 
+         type2 == ProblemType::CONVEX_GLWS ? "Convex GLWS" : "Unknown") 
         << std::endl;
+
     
+    // // Example 3: Manual problem definition for Convex GLWS
+    // auto glws_problem = ProblemBuilder<long double>::create()
+    //     .withVar("i", 0, 20)
+    //     .withVar("j", 0, 20)
+    //     .withObjective(Objective::MINIMIZE)
+    //     .withConstraint("i", "j", Constraint::ConstraintType::LESS_THAN)
+    //     .withSequence("data", std::vector<long double>{1.0, 2.5, 3.0, 4.2, 5.1, 6.3})
+    //     .withValue("buildCost", 10.0)
+    //     .withRecurrence([](const std::map<std::string, int>& state) {
+    //         // Recurrence relation for convex GLWS
+    //         // dp[i][j] = min(dp[i][k] + cost(k, j)) for all k in [i, j)
+    //     })
+    //     .build();
+    
+    // double glws_result = SolverDispatcher::solve<long double>(glws_problem);
+    // std::cout << "Convex GLWS result: " << glws_result << std::endl;
     return 0;
 }
